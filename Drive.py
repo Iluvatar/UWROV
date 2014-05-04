d823ed44cfa8cfb61c98f28475308ed110112fa0
Ryan Cox <ryanedwincox@gmail.com>
2014-05-04 01:08:12 -0700

    added ROV top view

--- Drive.py
+++ Drive.py
@@ -1,10 +1,13 @@
 # NOTE: this is not fully functional yet. As in, it still doesn't work properly.
 
-import serial, math;
+# import serial, math;
+import math
 # from enum import Enum;
-from serial import Serial, SerialException, SerialTimeoutException;
+# from serial import Serial, SerialException, SerialTimeoutException;
 from time import sleep;
 from OrcusGUI import OrcusGUI
+from MOTOR import MOTOR
+# NOTE: this is not fully functional yet. As in, it still doesn't work properly.
 
 
 """
@@ -25,24 +28,199 @@ from OrcusGUI import OrcusGUI
 """
 
 class Motor:
-	def __init__(self, ID, pow_header, dir_header, power = 0, dir = 0):
+	def __init__(self, ID, pow_header, dir_header, power = 0):
 		self.ID = ID;
 		self.pow_header = pow_header;
 		self.dir_header = dir_header;
 		self.power = power;
-		self.dir = dir;
 
-class MOTOR():
-	FR_LF = 1; # front left
-	FR_RT = 2; # front right
-	BA_RT = 3; # back right
-	BA_LF = 4; # back left
-	FR_VT = 5; # front vertical
-	BA_VT = 6; # back vertical
+# class MOTOR(Enum):
+	# FR_LF = 1; # front left
+	# FR_RT = 2; # front right
+	# BA_RT = 3; # back right
+	# BA_LF = 4; # back left
+	# FR_VT = 5; # front vertical
+	# BA_VT = 6; # back vertical
+
+class Control:
+	def __init__(self, trans_x = 0, trans_y = 0, yaw = 0, rise = 0,
+				 trans_x_tare = 0, trans_y_tare = 0, yaw_tare = 0, rise_tare = 0):
+		self.trans_x = trans_x;
+		self.trans_y = trans_y;
+		self.yaw = yaw;
+		self.rise = rise;
+		self.trans_x_tare = trans_x_tare;
+		self.trans_y_tare = trans_y_tare;
+		self.yaw_tare = yaw_tare;
+		self.rise_tare = rise_tare;
+	
+	def tare(self):
+		self.trans_x_tare = self.trans_x;
+		self.trans_y_tare = self.trans_y;
+		self.yaw_tare = self.yaw;
+		self.rise_tare = self.rise;
+	
+	def trans_x_value(self):
+		return self.trans_x - self.trans_x_tare;
+		
+	def trans_y_value(self):
+		return self.trans_y - self.trans_y_tare;
+		
+	def yaw_value(self):
+		return self.yaw - self.yaw_tare;
+		
+	def rise_value(self):
+		return self.rise - self.rise_tare;
 
 
-# range -1 to 1
-trans_x, trans_y, yaw, rise = 0, 0, 0, 0;
+
+# def connect(port_name):
+	# """Returns a Serial object that is connected to the port_name. Returns
+	# None if the connection could not be made."""
+	
+	# try:
+		# return Serial(port_name, timeout = .5, writeTimeout = .5);
+	# except SerialException:
+		# print("connect: could not connect to port " + port_name);
+		# return None;
+
+
+# def read_data_values(ser, data_values):
+	# """Reads data from ser and updates the dict data_values.
+	
+	# The keys in data_values should be bytes that are the headers for sensor
+	# values. Attempts to find each key in the read data and then updates it if
+	# the next two bytes are consistent."""
+	
+	# if not isinstance(data_values, dict):
+		# raise ValueError("read_data_values: data_values not of type dict");
+	# if not isinstance(ser, Serial):
+		# raise ValueError("read_data_values: ser not of type Serial");
+		
+	# # so we don't read too much data
+	# read_chars = min(ser.inWaiting(), len(data_values) * 5);
+	# raw_data = ser.read(read_chars);
+	
+	# for key in data_values.keys():
+		# pos = raw_data[:-1].rfind(key);
+		# if pos != -1:
+			# data_values[key] = raw_data[pos + 1];
+	
+	# if read_chars > 12:
+		# ser.flushInput();
+
+
+# def write_motor_values(ser):
+	# """Writes the motor power and direction to the serial port.
+	
+	# Each write consists of a header followed by the value repeated twice."""
+	
+	# # do something about this
+	# global motors;
+	
+	# if not isinstance(ser, Serial):
+		# raise ValueError("write_motor_values: ser not of type Serial")
+	# for motor in motors:
+		# pow = motors[motor].power;
+		# dir = b'1' if pow > 0 else b'0';
+		# ser.write(motors[motor].pow_header + bytes([abs(pow)] * 2));
+		# ser.write(motors[motor].dir_header + dir * 2);
+
+
+
+
+
+def update_motor_values(control):
+	# do something about thisself.
+	global motors;
+	
+	for motor in motors:
+		motors[motor].power = get_motor_power(motor, control);
+
+def get_motor_power(n, control):
+	"""Takes the motor number and returns the magnitude of the total power it
+	should output, from -255 to 255. Returns 0 if an invalid motor is given."""
+	
+	power_sum = get_trans_power(n, control) + get_yaw_power(n, control) + \
+				get_rise_power(n, control);
+	scaled_power = int(power_sum * 255);
+	try:
+		return min(scaled_power, 255) if scaled_power > 0 else max(scaled_power, -255);
+	except ValueError as bad_motor:
+		print(bad_motor.args[0]);
+		return 0;
+
+
+def get_trans_power(n, control):
+	"""Takes the motor number and returns the power it should output for
+	translational motion, from -1 to 1.
+	
+	Raises a ValueError if the motor number is unrecognized."""
+	
+	# these motors don't have an effect on translational speed
+	if n == MOTOR.FR_VT or n == MOTOR.BA_VT:
+		return 0;
+	if n == MOTOR.FR_LF or n == MOTOR.BA_RT:
+		# TODO implement
+		return -1 * control.trans_y_value();
+	if n == MOTOR.FR_RT or n == MOTOR.BA_LF:
+		# TODO implement
+		return control.trans_y_value();
+	raise ValueError("get_trans_power: Illegal motor number");
+
+
+def get_yaw_power(n, control):
+	"""Takes the motor number and returns the power it should output for
+	rotational motion, from -1 to 1.
+	
+	Raises a ValueError if the motor number is unrecognized."""
+	
+	# these motors don't have an effect on translational speed
+	if n == MOTOR.FR_VT or n == MOTOR.BA_VT:
+		return 0;
+	if n == MOTOR.FR_LF or n == MOTOR.BA_RT:
+		return control.yaw_value();
+	if n == MOTOR.FR_RT or n == MOTOR.BA_LF:
+		return -1 * control.yaw_value();
+	raise ValueError("get_yaw_power: Illegal motor number");
+
+
+def get_rise_power(n, control):
+	"""Takes the motor number and returns the power it should output for
+	vertical motion, from -1 to 1.
+	
+	Raises a ValueError if the motor number is unrecognized."""
+	
+	# these motors don't have an effect on translational speed
+	if n == MOTOR.FR_LF or n == MOTOR.BA_RT or \
+	   n == MOTOR.FR_RT or n == MOTOR.BA_LF:
+		return 0;
+	if n == MOTOR.FR_VT or n == MOTOR.BA_VT:
+		return control.rise_value();
+	raise ValueError("get_rise_power: Illegal motor number");
+
+
+
+
+# def update_joy_values(joystick, control):
+	# control.trans_x = joystick.get_axis(0);
+	# control.trans_y = -1 * joystick.get_axis(1);
+	# control.rise = -1 * joystick.get_axis(3);
+	# control.yaw = joystick.get_axis(4);
+
+# def process_joy_events():
+	# for event in pygame.event.get():
+		# if event.type == pygame.JOYBUTTONDOWN and event.__dict__["button"] == 1:
+			# control.tare();
+
+
+
+
+
+
+
+ser = None;
+control = Control();
 
 motors = {MOTOR.FR_LF: Motor(MOTOR.FR_LF, b'1', b'a'),
 		  MOTOR.FR_RT: Motor(MOTOR.FR_RT, b'2', b'b'),
@@ -57,141 +235,49 @@ sensor_values = {b'P': 0, b'H': 0, b'T': 0, b'C': 0};
 
 
 
-def connect(port_name):
-	"""Returns a Serial object that is connected to the port_name. Returns
-	None if the connection could not be made."""
+# def joy_init():
+	# """Initializes pygame and the joystick, and returns the joystick to be
+	# used."""
 	
-	try:
-		return Serial(port_name, timeout = .05, writeTimeout = .05);
-	except SerialException:
-		print("connect: could not connect to port " + port_name);
-		return None;
+	# pygame.init();
+	# pygame.joystick.init();
+	# if pygame.joystick.get_count() == 0:
+		# raise Exception("joy_init: No joysticks connected");
+	# joystick = pygame.joystick.Joystick(0);
+	# joystick.init();
+	# return joystick;
 
 
-def read_data_values(ser, data_values):
-	"""Reads data from ser and updates the dict data_values.
-	
-	The keys in data_values should be bytes that are the headers for sensor
-	values. Attempts to find each key in the read data and then updates it if
-	the next two bytes are consistent."""
-	
+def onexit():
+	control.trans_x, control.trans_y, control.yaw, control.rise = 0, 0, 0, 0;
+	control.trans_x_tare, control.trans_y_tare = 0, 0;
+	control.yaw_tare, control.rise_tare = 0, 0;
+	update_motor_values();
+	write_motor_values(ser);
+
+
+def print_data_values(data_values):
 	if not isinstance(data_values, dict):
-		raise ValueError("read_data_values: data_values not of type dict")
-	if not isinstance(ser, Serial):
-		raise ValueError("read_data_values: ser not of type Serial")
-		
-	# so we don't read too much data
-	read_chars = min(ser.inWaiting(), len(data_values) * 5);
-	raw_data = ser.read(read_chars);
-	
-	for key in data_values.keys():
-		pos = raw_data[:-2].rfind(key);
-		if pos != -1:
-			bit1 = key[pos + 1];
-			bit2 = key[pos + 2];
-			if bit1 == bit2:
-				data_values[key] = bit1;
-	
-	if read_chars > 12:
-		ser.flushInput();
-
-
-def write_motor_values(ser):
-	"""Writes the motor power and direction to the serial port.
-	
-	Each write consists of a header followed by the value repeated twice."""
-	
-	# do something about this
-	global motors;
-	
-	if not isinstance(ser, Serial):
-		raise ValueError("write_motor_values: ser not of type Serial")
-	for motor in motors:
-		pow = get_motor_power(motor);
-		dir = b'1' if pow > 0 else b'0';
-		ser.write(motors[motor].pow_header + bytes([abs(pow)] * 2));
-		ser.write(motors[motor].dir_header + dir * 2);
-
-def get_motor_power(n):
-	"""Takes the motor number and returns the magnitude of the total power it
-	should output, from -255 to 255. Returns 0 if an invalid motor is given."""
-	
-	try:
-		return (get_trans_power(n) + get_yaw_power(n) + get_rise_power(n)) * 255;
-	except ValueError as bad_motor:
-		print(bad_motor.args[0]);
-		return 0;
-
-
-def get_trans_power(n):
-	"""Takes the motor number and returns the power it should output for
-	translational motion, from -1 to 1.
-	
-	Raises a ValueError if the motor number is unrecognized."""
-	
-	# do something about this
-	global trans_x, trans_y;
-	
-	# these motors don't have an effect on translational speed
-	if n == MOTOR.FR_VT or n == MOTOR.BA_VT:
-		return 0;
-	if n == MOTOR.FR_LF or n == MOTOR.BA_RT:
-		# TODO implement
-		print("Finish get_trans_power");
-		return -1 * trans_y;
-	if n == MOTOR.FR_RT or n == MOTOR.BA_LF:
-		# TODO implement
-		print("Finish get_trans_power");
-		return trans_y;
-	raise ValueError("get_trans_power: Illegal motor number");
-
-
-def get_yaw_power(n):
-	"""Takes the motor number and returns the power it should output for
-	rotational motion, from -1 to 1.
-	
-	Raises a ValueError if the motor number is unrecognized."""
-	
-	# do something about this
-	global yaw;
-	
-	# these motors don't have an effect on translational speed
-	if n == MOTOR.FR_VT or n == MOTOR.BA_VT:
-		return 0;
-	if n == MOTOR.FR_LF or n == MOTOR.BA_RT:
-		return yaw;
-	if n == MOTOR.FR_RT or n == MOTOR.BA_LF:
-		return -1 * yaw;
-	raise ValueError("get_yaw_power: Illegal motor number");
-
-
-def get_rise_power(n):
-	"""Takes the motor number and returns the power it should output for
-	vertical motion, from -1 to 1.
-	
-	Raises a ValueError if the motor number is unrecognized."""
-	
-	# do something about this
-	global rise;
-	
-	# these motors don't have an effect on translational speed
-	if n == MOTOR.FR_LF or n == MOTOR.BA_RT or \
-	   n == MOTOR.FR_RT or n == MOTOR.BA_LF:
-		return 0;
-	if n == MOTOR.FR_VT or n == MOTOR.BA_VT:
-		return rise;
-	raise ValueError("get_rise_power: Illegal motor number");
+		raise ValueError("print_data_values: data_values not of type dict");
+	pprint(data_values);
 
 def mainDrive():
-	# do something about this
-	global sensor_values;
+	global control;
 	
-   # ser = connect("COM3");
- 		
-	# print("test")
-	# print gui.printMotorValues()
-	trans_x = gui.frontLeft / 100.0 
-	print ("trans_x" + str(trans_x))
+	# joystick = joy_init();
+	# ser = connect("COM3");
+	
+	# update_joy_values(joystick, control);
+	# update_motor_values(control);
+	
+	# sets the motor values to the sliders
+	motors[MOTOR.FR_LF].power = gui.frontLeft * 2.5 - 127
+	motors[MOTOR.FR_RT].power = gui.frontRight * 2.5 - 127
+	motors[MOTOR.BA_LF].power = gui.backLeft * 2.5 - 127 
+	motors[MOTOR.BA_RT].power = gui.backRight * 2.5 - 127 
+	motors[MOTOR.FR_VT].power = gui.frontVert * 2.5 - 127
+	motors[MOTOR.BA_VT].power = gui.backVert * 2.5 - 127
+	
 	gui.drawMotorStatus(motors)
 	
 	# try:
@@ -200,8 +286,17 @@ def mainDrive():
 		# print("write timeout");
 	
 	# read_data_values(ser, sensor_values);
-
+	# #print_data_values(sensor_values);
+	
+	# process_joy_events();
+	
 	gui.after(1, mainDrive) # loops the mainDrive method
+	
+
+
+# atexit.register(onexit);
+
+
 
 gui = OrcusGUI()		
 gui.master.geometry("500x500")			   
--- OrcusGUI.py
+++ OrcusGUI.py
@@ -1,16 +1,18 @@
-import Tkinter as tk	  
-import serial, math;
-# from enum import Enum;
-from serial import Serial, SerialException, SerialTimeoutException;
+import Tkinter as tk	
+import math  
+# import serial, math;
+# # from enum import Enum;
+# from serial import Serial, SerialException, SerialTimeoutException;
 from time import sleep;
+from MOTOR import MOTOR
 
-class MOTOR():
-	FR_LF = 1; # front left
-	FR_RT = 2; # front right
-	BA_RT = 3; # back right
-	BA_LF = 4; # back left
-	FR_VT = 5; # front vertical
-	BA_VT = 6; # back vertical
+# class MOTOR():
+	# FR_LF = 1; # front left
+	# FR_RT = 2; # front right
+	# BA_RT = 3; # back right
+	# BA_LF = 4; # back left
+	# FR_VT = 5; # front vertical
+	# BA_VT = 6; # back vertical
 
 class OrcusGUI(tk.Frame):			  
 	def __init__(self, master=None):
@@ -22,8 +24,10 @@ class OrcusGUI(tk.Frame):
 		self.frontVert = 0
 		self.backVert = 0
 		
-		self.C = tk.Canvas(self, height=275, width=200)
-		self.rov = self.C.create_rectangle(20,20,180,255, fill='#4c0099')
+		self.C = tk.Canvas(self, height=235, width=169)
+		self.rovTop = tk.PhotoImage(file='./orcusTop.gif')
+		#self.rov = self.C.create_rectangle(20,20,180,255, fill='#4c0099')
+		self.rov = self.C.create_image(1,1,image=self.rovTop, anchor='nw')
 		self.frontLeftLine = self.C.create_line(0,0,0,0)
 		self.frontRightLine = self.C.create_line(0,0,0,0)
 		self.backLeftLine = self.C.create_line(0,0,0,0)
@@ -103,19 +107,13 @@ class OrcusGUI(tk.Frame):
 		self.C.delete(self.frontVertLine)
 		self.C.delete(self.backVertLine)
 		
-		print str(motors[MOTOR.FR_LF].power)
+		print (str(motors[MOTOR.FR_LF].power))
 		
-		self.frontLeftLine = self.C.create_line(LEFT_X, FRONT_Y, LEFT_X+0.5*motors['motor.FR_LF'].power-50), FRONT_Y-0.866*(motors['motor.FR_LF'].power), width=10)
-		self.frontRightLine = self.C.create_line(RIGHT_X, FRONT_Y, RIGHT_X-0.5*(int(self.frontRight)-50), FRONT_Y-0.866*(int(self.frontRight)-50), width=10)
-		self.backLeftLine = self.C.create_line(LEFT_X, BACK_Y, LEFT_X+0.5*(int(self.backLeft)-50), BACK_Y+0.866*(int(self.backLeft)-50), width=10)
-		self.backRightLine = self.C.create_line(RIGHT_X, BACK_Y, RIGHT_X-0.5*(int(self.backRight)-50), BACK_Y+0.866*(int(self.backRight)-50), width=10)
-		self.frontVertLine = self.C.create_line(VERT_X, FRONT_VERT_Y, VERT_X, FRONT_VERT_Y-50+int(self.frontVert), width=10)
-		self.backVertLine = self.C.create_line(VERT_X, BACK_VERT_Y, VERT_X, BACK_VERT_Y-50+int(self.backVert), width=10)
+		self.frontLeftLine = self.C.create_line(LEFT_X, FRONT_Y, LEFT_X+0.5*motors[MOTOR.FR_LF].power/2, FRONT_Y-0.866*motors[MOTOR.FR_LF].power/2, width=10)
+		self.frontRightLine = self.C.create_line(RIGHT_X, FRONT_Y, RIGHT_X-0.5*motors[MOTOR.FR_RT].power/2, FRONT_Y-0.866*motors[MOTOR.FR_RT].power/2, width=10)
+		self.backLeftLine = self.C.create_line(LEFT_X, BACK_Y, LEFT_X+0.5*motors[MOTOR.BA_LF].power/2, BACK_Y+0.866*motors[MOTOR.BA_LF].power/2, width=10)
+		self.backRightLine = self.C.create_line(RIGHT_X, BACK_Y, RIGHT_X-0.5*motors[MOTOR.BA_RT].power/2, BACK_Y+0.866*motors[MOTOR.BA_RT].power/2, width=10)
+		self.frontVertLine = self.C.create_line(VERT_X, FRONT_VERT_Y, VERT_X, FRONT_VERT_Y+motors[MOTOR.FR_VT].power/2, width=10)
+		self.backVertLine = self.C.create_line(VERT_X, BACK_VERT_Y, VERT_X, BACK_VERT_Y+motors[MOTOR.BA_VT].power/2, width=10)
 		
-		# self.frontLeftLine = self.C.create_line(LEFT_X, FRONT_Y, LEFT_X+0.5*(int(self.frontLeft)-50), FRONT_Y-0.866*(int(self.frontLeft)-50), width=10)
-		# self.frontRightLine = self.C.create_line(RIGHT_X, FRONT_Y, RIGHT_X-0.5*(int(self.frontRight)-50), FRONT_Y-0.866*(int(self.frontRight)-50), width=10)
-		# self.backLeftLine = self.C.create_line(LEFT_X, BACK_Y, LEFT_X+0.5*(int(self.backLeft)-50), BACK_Y+0.866*(int(self.backLeft)-50), width=10)
-		# self.backRightLine = self.C.create_line(RIGHT_X, BACK_Y, RIGHT_X-0.5*(int(self.backRight)-50), BACK_Y+0.866*(int(self.backRight)-50), width=10)
-		# self.frontVertLine = self.C.create_line(VERT_X, FRONT_VERT_Y, VERT_X, FRONT_VERT_Y-50+int(self.frontVert), width=10)
-		# self.backVertLine = self.C.create_line(VERT_X, BACK_VERT_Y, VERT_X, BACK_VERT_Y-50+int(self.backVert), width=10)
 
